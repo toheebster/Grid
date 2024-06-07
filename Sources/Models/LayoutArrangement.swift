e0610ddb4d93b9bd988c457c17dd08c240fb7a63
toheebster <okenlatoheeb@yahoo.com>
2024-06-07 16:55:01 -0500

    Optimizing layout arrangement with hashmap

--- Sources/Models/LayoutArrangement.swift
+++ Sources/Models/LayoutArrangement.swift
@@ -13,10 +13,21 @@ struct LayoutArrangement: Equatable, Hashable {
   var columnsCount: Int
   var rowsCount: Int
   let items: [ArrangedItem]
+  private var _itemsMap: [AnyHashable?: ArrangedItem]
+  
+  init(columnsCount: Int, rowsCount: Int, items: [ArrangedItem]) {
+    self.columnsCount = columnsCount
+    self.rowsCount = rowsCount
+    self.items = items
+    var __items = Dictionary<AnyHashable?, ArrangedItem>(minimumCapacity: items.count)
+    items.forEach { __items[$0.gridElement.id] = $0 }
+    self._itemsMap = __items
+  }
   
   static var zero = LayoutArrangement(columnsCount: 0, rowsCount: 0, items: [])
   
   subscript(gridElement: GridElement) -> ArrangedItem? {
-    items.first(where: { $0.gridElement == gridElement })
+//    items.first(where: { $0.gridElement == gridElement })
+    _itemsMap[gridElement.id]
   }
 }
